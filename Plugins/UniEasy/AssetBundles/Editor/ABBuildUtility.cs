5f24326fcb760e277c4460188e47c73a4a2ee0cb
chaolun <chaolun.w@redrabbitanimation.cn>
2019-07-15 20:13:09 +0800

    - Start work on AssetBundles Manager

---
+++ Plugins/UniEasy/AssetBundles/Editor/ABBuildUtility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using System.IO;
+
+namespace UniEasy.Editor
+{
+    public class ABBuildUtility
+    {
+        [MenuItem("Tools/AssetBundles/Build StandaloneWindows64")]
+        public static void BuildAllABForWindows64()
+        {
+            BuildAllAB(BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+        }
+
+        [MenuItem("Tools/AssetBundles/Build iOS")]
+        public static void BuildAllABForIOS()
+        {
+            BuildAllAB(BuildAssetBundleOptions.None, BuildTarget.iOS);
+        }
+
+        [MenuItem("Tools/AssetBundles/Build Android")]
+        public static void BuildAllABForAndroid()
+        {
+            BuildAllAB(BuildAssetBundleOptions.None, BuildTarget.Android);
+        }
+
+        private static void BuildAllAB(BuildAssetBundleOptions options, BuildTarget target)
+        {
+            var outPath = PathsUtility.GetABOutPath();
+            if (!Directory.Exists(outPath))
+            {
+                Directory.CreateDirectory(outPath);
+            }
+            BuildPipeline.BuildAssetBundles(outPath, options, target);
+            AssetDatabase.Refresh();
+        }
+    }
+}
---
+++ Plugins/UniEasy/AssetBundles/Editor/ABDeleteUtility.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using System.IO;
+
+namespace UniEasy.Editor
+{
+    public class ABDeleteUtility
+    {
+        [MenuItem("Tools/AssetBundles/Delete All")]
+        public static void DeleteAllAB()
+        {
+            var outPath = PathsUtility.GetABOutPath();
+            if (!string.IsNullOrEmpty(outPath))
+            {
+                Directory.Delete(outPath, true);
+                File.Delete(outPath + ".meta");
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
