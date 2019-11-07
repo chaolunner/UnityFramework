55ad5c2fd1aff0d5f543a1d4ee42218c79390422
chaolun <chaolun.w@redrabbitanimation.cn>
2019-11-07 12:58:26 +0800

    - Optimize network subject

--- Plugins/UniEasy/Net/Core/NetworkBroker.cs
+++ Plugins/UniEasy/Net/Core/NetworkBroker.cs
@@ -8,10 +8,40 @@ namespace UniEasy.Net
 {
     public interface ISubject<T> : IObservable<T>, IObserver<T>, IDisposable { }
 
+    public class EmptyDisposable : IDisposable
+    {
+        public static EmptyDisposable Singleton = new EmptyDisposable();
+        private EmptyDisposable() { }
+        public void Dispose() { }
+    }
+
     public class Subject<T> : ISubject<T>
     {
+        class Subscription : IDisposable
+        {
+            Subject<T> parent;
+            IObserver<T> unsubscribeTarget;
+
+            public Subscription(Subject<T> parent, IObserver<T> unsubscribeTarget)
+            {
+                this.parent = parent;
+                this.unsubscribeTarget = unsubscribeTarget;
+            }
+
+            public void Dispose()
+            {
+                if (parent != null)
+                {
+                    parent.observerList.Remove(unsubscribeTarget);
+                    unsubscribeTarget = null;
+                    parent = null;
+                }
+            }
+        }
+
         private bool isDisposed;
-        private List<IObserver<T>> subjectList = new List<IObserver<T>>();
+        private bool isStopped;
+        private List<IObserver<T>> observerList = new List<IObserver<T>>();
         private Action onCompleted = null;
         private Action<Exception> onError = null;
         private Action<T> onNext = null;
@@ -25,41 +55,58 @@ namespace UniEasy.Net
 
         public void OnCompleted()
         {
-            foreach (var subject in subjectList)
+            foreach (var observer in observerList)
             {
-                subject.OnCompleted();
+                observer.OnCompleted();
             }
-            if (!isDisposed) { onCompleted?.Invoke(); }
+            if (!isStopped && !isDisposed) { onCompleted?.Invoke(); }
+            isStopped = true;
         }
 
         public void OnError(Exception error)
         {
-            foreach (var subject in subjectList)
+            foreach (var observer in observerList)
             {
-                subject.OnError(error);
+                observer.OnError(error);
             }
-            if (!isDisposed) { onError?.Invoke(error); }
+            if (!isStopped && !isDisposed) { onError?.Invoke(error); }
+            isStopped = true;
         }
 
         public void OnNext(T value)
         {
-            foreach (var subject in subjectList)
+            foreach (var observer in observerList)
             {
-                subject.OnNext(value);
+                observer.OnNext(value);
             }
-            if (!isDisposed) { onNext?.Invoke(value); }
+            if (!isStopped && !isDisposed) { onNext?.Invoke(value); }
         }
 
         public IDisposable Subscribe(IObserver<T> observer)
         {
-            var subject = observer as ISubject<T>;
-            subjectList.Add(subject);
-            return subject;
+            try
+            {
+                if (!isStopped)
+                {
+                    observerList.Add(observer);
+                    return new Subscription(this, observer);
+                }
+                else
+                {
+                    observer.OnCompleted();
+                }
+            }
+            catch (Exception e)
+            {
+                observer.OnError(e);
+            }
+            return EmptyDisposable.Singleton;
         }
 
         public void Dispose()
         {
             isDisposed = true;
+            observerList.Clear();
             onCompleted = null;
             onError = null;
             onNext = null;
@@ -85,7 +132,7 @@ namespace UniEasy.Net
     {
         void Connect();
         void Disconnect();
-        void RunOnMainThread();
+        void Update();
     }
 
     public class NetworkBroker : INetworkBroker, IDisposable
@@ -130,7 +177,7 @@ namespace UniEasy.Net
             }
         }
 
-        public void RunOnMainThread()
+        public void Update()
         {
             if (runOnMainThread.Count > 0)
             {
--- Plugins/UniEasy/Net/Installers/NetworkInstaller.cs
+++ Plugins/UniEasy/Net/Installers/NetworkInstaller.cs
@@ -4,20 +4,25 @@ namespace UniEasy.Net
 {
     public class NetworkInstaller : MonoInstaller
     {
+        private INetworkBroker networkBroker;
+        private INetworkSystem networkSystem;
+
         public override void InstallBindings()
         {
             Container.Bind<INetworkBroker>().To<NetworkBroker>().AsSingle();
             Container.Bind<INetworkSystem>().To<NetworkSystem>().AsSingle();
+            networkBroker = Container.Resolve<INetworkBroker>();
+            networkSystem = Container.Resolve<INetworkSystem>();
         }
 
         private void Update()
         {
-            Container.Resolve<INetworkBroker>().RunOnMainThread();
+            networkBroker.Update();
         }
 
         private void OnDestroy()
         {
-            Container.Resolve<INetworkSystem>().Dispose();
+            networkSystem.Dispose();
         }
     }
 }
