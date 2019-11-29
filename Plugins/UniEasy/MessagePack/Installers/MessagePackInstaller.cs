d6f47e511c3a572778dceeb6099a2349ce06da30
chaolun <chaolun.w@redrabbitanimation.cn>
2019-11-29 16:33:41 +0800

    - Fixed build target uses IL2CPP, you can not use Dynamic***Resolver so require to use pre-code generation problem - Subproject commit 492820260d00c40b193301f353a71a410c57362d

---
+++ Plugins/UniEasy/MessagePack/Installers/MessagePackInstaller.cs
@@ -0,0 +1,16 @@
+﻿using MessagePack.Resolvers;
+using MessagePack;
+using UniEasy.DI;
+
+namespace UniEasy
+{
+    public class MessagePackInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            StaticCompositeResolver.Register(GeneratedResolver.Instance, StandardResolver.Instance);
+            var options = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
+            MessagePackSerializer.DefaultOptions = options;
+        }
+    }
+}
