17a9d6e4f310a39b0f7a481f9dc2aba8b6e2ea5e
chaolun <chaolun.w@redrabbitanimation.cn>
2019-12-02 13:41:17 +0800

    - Make MessagePack don't need generate code in editor

--- Plugins/UniEasy/MessagePack/Installers/MessagePackInstaller.cs
+++ Plugins/UniEasy/MessagePack/Installers/MessagePackInstaller.cs
@@ -8,9 +8,11 @@ namespace UniEasy
     {
         public override void InstallBindings()
         {
+#if !UNITY_EDITOR
             StaticCompositeResolver.Register(GeneratedResolver.Instance, StandardResolver.Instance);
             var options = MessagePackSerializerOptions.Standard.WithResolver(StaticCompositeResolver.Instance);
             MessagePackSerializer.DefaultOptions = options;
+#endif
         }
     }
 }
