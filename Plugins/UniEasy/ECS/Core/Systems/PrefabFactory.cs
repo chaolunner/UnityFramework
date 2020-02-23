4ab1163923133b22b1d837c3620480113e9fd44a
chaolun <0chaolun0@gmail.com>
2020-02-23 13:48:02 +0800

    - Optimize Pool Factory

--- Plugins/UniEasy/ECS/Core/Systems/PrefabFactory.cs
+++ Plugins/UniEasy/ECS/Core/Systems/PrefabFactory.cs
@@ -9,14 +9,24 @@ namespace UniEasy.ECS
         [Inject]
         protected DiContainer Container = null;
 
+        public GameObject Instantiate(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent = null, Action<GameObject> action = null)
+        {
+            return Instantiate(prefab, position, rotation, parent, false, action);
+        }
+
         public GameObject Instantiate(GameObject prefab, Transform parent = null, bool worldPositionStays = false, Action<GameObject> action = null)
+        {
+            return Instantiate(prefab, Vector3.zero, Quaternion.identity, parent, worldPositionStays, action);
+        }
+
+        private GameObject Instantiate(GameObject prefab, Vector3 position, Quaternion rotation, Transform parent, bool worldPositionStays, Action<GameObject> action)
         {
             var wasActive = prefab.activeSelf;
             if (wasActive)
             {
                 prefab.SetActive(false);
             }
-            var go = GameObject.Instantiate(prefab, parent, worldPositionStays);
+            var go = worldPositionStays ? GameObject.Instantiate(prefab, parent, true) : GameObject.Instantiate(prefab, position, rotation, parent);
             go.name = prefab.name;
             action?.Invoke(go);
             if (wasActive)
